ff4c94cf8d43ea117d87aaeae31cf30090365390
Vilen-Tokarev <vilen.tokarev.2009@list.ru>
2024-11-23 16:39:24 +0300

    sigth16 fix

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -64,3 +64,19 @@ const card = document.getElementById('userDetails');
         } else {
             document.getElementById('userDetails').textContent = 'No user data found.';
         }
+
+
+
+        
+document.getElementById('burgerIcon').addEventListener('click', function () {
+    const menuItems = document.getElementById('menuItems');
+    const burgerIcon = document.getElementById('burgerIcon');
+
+    if (menuItems.classList.contains('open')) {
+        menuItems.classList.remove('open');
+        burgerIcon.classList.remove('open');
+    } else {
+        menuItems.classList.add('open');
+        burgerIcon.classList.add('open');
+    }
+});
